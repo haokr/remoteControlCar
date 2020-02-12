5104f56eaecff5f7a22bcdc580107b2206a5a82d
CreatorKarl <995199838@qq.com>
2020-02-12 20:25:28 +0800

    'auth'

--- auth.py
+++ auth.py
@@ -1,9 +1,10 @@
-from flask.ext.httpauth import httpBasicAuth
+from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
 from flask import g
 from models import User
+from utils.token import Token
 
-auth = httpBasicAuth()
-
+auth = HTTPBasicAuth()
+token_auth = HTTPTokenAuth(scheme="Bearer")
 
 @auth.verify_password
 def verify_password(username, password):
@@ -13,4 +14,17 @@ def verify_password(username, password):
     else:
         g.user = user
         return True
+
+@token_auth.verify_token
+def verify_token(token):
+    token_dict = Token.auth(token)
+    if token_dict:
+        g.user_id = token_dict.get('user_id')
+        g.car_ip = token_dict.get('car_ip')
+        g.car_port = token_dict.get('car_port')
+        g.token = token
+        return True
+    else:
+        return False
+
     
