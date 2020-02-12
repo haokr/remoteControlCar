4a24178e9ffdf56e1b9fc05bdf55651250106e2b
CreatorKarl <995199838@qq.com>
2020-02-12 20:25:03 +0800

    'token'

--- utils/token.py
+++ utils/token.py
@@ -7,42 +7,40 @@ from db import redis_cli
 DURATION = 60 * 30
 
 class Token():
-    def __init__(self, user_id, car_ip=='', car_port=''):
-        self.token = self._gen_token(user_id, car_ip, car_port)
-        self.user_id = user_id
-        self.car_ip = car_ip
-        self.car_port = car_port
-
-    def _gen_token(self, user_id, car_ip, car_port):
-        '''生成token并保存到Redis，token内容为：{"user_id":user_id, "car_ip":car_ip}
+    def __init__(self, user_id, car_id='', car_ip='', car_port=''):
+        '''生成token并保存到Redis，token: token:token_id:user_id:car_id,内容为：{"car_ip": ip, "car_port": port}
         '''
         token_id = shortuuid.uuid()
-        key = 'token:'+ token_id
-        pipe = redis_cli.pipe()
-        pipe.hmset(key, mapping={'user_id': user_id, 'car_ip': car_ip, "car_port": car_port})
+        key = f'token:{token_id}:{user_id}:{car_id}'
+        pipe = redis_cli.pipeline()
+        pipe.hmset(key, mapping={'car_ip': car_ip, "car_port": car_port})
         pipe.expire(key, DURATION)
         pipe.execute()
-        return token_id
+        self._token = token_id
+
+    def get(self):
+        return self._token
 
     @staticmethod
-    def token_auth(token):
+    def auth(token):
         '''验证token，如果token不存在返回False，如果token ttl小于DURATION值的一半重新设置expire
         '''
-        pipe = redis_cli.pipe()
-        token_dict = pipe.hgetall('token:'+token)
-
-        if not token_dict:
+        # 根据token获取key
+        matched_keys = redis_cli.keys(f'token:{token}:*:*')
+        if not matched_keys:
             return False
-        ttl = pipe.ttl("token:"+token)
-        if ttl < DURATION//2:
-            pipe.expire(token, DURATION)
-        pipe.execute()
+        key = matched_keys[0]
+        token_dict = redis_cli.hgetall(key)
 
-        if not token_dict.get('user_id'):
-            raise "Token have not user_id"
-        if not token_dict.get('car_ip') or not token_dict.get("car_port"):
-            raise "Token have not car_ip or car_port"
-        return True
+        ttl = redis_cli.ttl(key)
+        if ttl < DURATION//2:
+            redis_cli.expire(key, DURATION)
+
+        return {
+            'user_id': token_dict.get('user_id'),
+            'car_ip': token_dict.get('car_ip'),
+            'car_port': token_dict.get('car_port')
+        }
 
 
 
