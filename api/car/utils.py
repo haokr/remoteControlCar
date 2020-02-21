4bba4f35dad31b2d83109145b080179871bf7ed3
CreatorKarl <995199838@qq.com>
2020-02-21 16:38:27 +0800

    port auth

--- api/car/utils.py
+++ api/car/utils.py
@@ -9,7 +9,7 @@ def judge_legal_ip(one_str):
     判断一个字符串是否是合法IP地址 
     '''
     compile_ip = re.compile(
-        '^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?):\d{2,4}$')
+        '^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?):\d{2,5}$')
     if compile_ip.match(one_str):
         return True
     else:
